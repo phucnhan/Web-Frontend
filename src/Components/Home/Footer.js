654cfc6a8063ad430b916cfef31456c509a87f62
phucnhan <phucnhan0406@gmail.com>
2023-11-24 19:45:26 +0700

    update footer

--- src/Components/Home/Footer.js
+++ src/Components/Home/Footer.js
@@ -9,46 +9,40 @@ class Footer extends React.Component {
                     <div class="footer-item">
                         <h2 class="footer-title">ADDRESS</h2>
                         <div class="footer-items">
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing st18</h3>
+                            <h3>Ho Chi Minh</h3>
+                            <h3>Ha Noi</h3>
+                            <h3>Binh Duong</h3>
                         </div>
                     </div>
                     <div class="footer-item">
                         <h2 class="footer-title">SERVICES</h2>
                         <div class="footer-items">
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
+                            <h3>All our services</h3>
+                            <h3>FAQs</h3>
+                            <h3>Contact us</h3>
+                            <h3>Store Locator</h3>
                         </div>
                     </div>
                     <div class="footer-item">
-                        <h2 class="footer-title">SUPPLIERS</h2>
+                        <h2 class="footer-title">Legal & Corporate</h2>
                         <div class="footer-items">
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
+                            <h3>Terms of Use</h3>
+                            <h3>Privacy Notice</h3>
+                            <h3>Cookies Notice</h3>
+                            <h3>Impressum</h3>
+                            <h3>Jobs</h3>
                         </div>
                     </div>
                     <div class="footer-item">
-                        <h2 class="footer-title">INVESTMENT</h2>
+                        <h2 class="footer-title">FOLOW US</h2>
                         <div class="footer-items">
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
-                            <h3>Adipisicing elit.</h3>
+                            <h3>Facebook</h3>
+                            <h3>Tiktok</h3>
+                            <h3>Youtube</h3>
                         </div>
                     </div>
                 </div>
-                <div class="container end-footer">
-                    <div class="copyright">copyright © 2021 - Present • <b>DOWNTOWN TECH LIVER</b></div>
-                    <a class="designer" href="#">Thierry M</a>
-                </div>
+
             </div>
         )
     }
