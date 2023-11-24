beddeeef141c190d7290e77d62397c8be8afd357
Thanh Binh <binhnguyenn2410@gmail.com>
2023-11-24 20:23:30 +0700

    1
    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Shop from './Components/Home/Shop';
+import Login from './Components/Login/Login';
 function App() {
   return (
     <div className="App">
       <header className="App-home">
         <Shop />
+        
       </header>
     </div>
   );
