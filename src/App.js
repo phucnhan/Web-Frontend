5f737c083f0388413421f210812b9dcbf17f0b6c
Thanh Binh <binhnguyenn2410@gmail.com>
2023-11-24 21:46:09 +0700

    big
    BIG UPDATE

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Shop from './Components/Home/Shop';
-import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Shop" element={<Shop />} />
-      </Routes>
-    </Router>
+    <div className="App">
+        <Shop />
+    </div>
   );
 }
-
 export default App;
