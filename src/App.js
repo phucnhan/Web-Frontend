c1eb0ce4a3ab8292c2a4001cd04124b077dd80d6
phucnhan <phucnhan0406@gmail.com>
2023-12-08 03:14:15 +0700

    1
    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import './App.css';
@@ -7,16 +7,38 @@ import About from './Components/About/About';
 import Contact from './Components/Contact Us/Contact';
 import Shop from './Components/Shop/Shop';
 import Cart from './Components/Cart/Cart';
+
+// Example: A simple authentication check function
+const isAuthenticated = () => {
+  // Implement your authentication logic here
+  // Return true if the user is authenticated, otherwise false
+  // You may want to check if the user has a valid token, session, etc.
+  return false; // For demonstration purposes, always redirect to login
+};
+
+// ProtectedRoute component for handling authenticated routes
+const ProtectedRoute = ({ element, path }) => {
+  if (isAuthenticated()) {
+    return element;
+  } else {
+    // Redirect to the login page if not authenticated
+    return <Navigate to="/Login" />;
+  }
+};
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Use ProtectedRoute for routes that require authentication */}
+        <Route
+          path="/"
+          element={<div className="app-container"><ProtectedRoute element={<Home />} /></div>}
+        />
         <Route path="/Login" element={<div className="app-container-login"><Login /></div>} />
-        <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
         <Route path='/About' element={<div className="app-container-about"><About /></div>} />
         <Route path='/Contact' element={<div className="app-container-about"><Contact /></div>} />
-        <Route path="/Shop" element={<Shop />} />
+        <Route path="/Shop" element={<div className="app-container"><ProtectedRoute element={<Shop />} /></div>} />
         <Route path='/Cart' element={<div className="app-container-about"><Cart /></div>} />
       </Routes>
     </Router>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Login.css";
-import Navbar from "../Home/Navbar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGooglePlusG, faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
@@ -9,58 +8,114 @@ import Footer from "../Home/Footer";
 library.add(faGooglePlusG, faFacebookF, faGithub, faLinkedinIn);
 
 class Login extends React.Component {
-
-    componentDidUpdate() {
-        const container = document.getElementById('container');
-        const signUpBtn = document.getElementById('register');
-        const signInBtn = document.getElementById('login');
-
-        if (container && signUpBtn && signInBtn) {
-            signUpBtn.addEventListener('click', () => {
-                container.classList.add("active");
-            });
-
-            signInBtn.addEventListener('click', () => {
-                container.classList.remove("active");
-            });
-        }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "", // Updated from 'username'
+            email: "",
+            password: "",
+            redirectToHome: false,
+        };
     }
+
+    handleInputChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    handleSignUp = (e) => {
+        e.preventDefault();
+        const { name, email, password } = this.state;
+
+        // Make sure 'name', 'email', and 'password' are provided
+        if (!name || !email || !password) {
+            console.error("Name, email, and password are required");
+            return;
+        }
+
+        // Send registration data to the backend
+        // (You can use fetch or any other method to send the data)
+        // Example using fetch:
+        fetch("http://localhost:3001/api/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, email, password }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                // Handle the response as needed
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
 
     render() {
         return (
             <div className="login-page">
-                <Navbar />
+                {/* Your existing code for Navbar */}
+
                 <div className="login-container" id="container">
                     <div className="form-container sign-up">
                         <form>
                             <h1>Create Account</h1>
-                            <div className="social-icons">
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'google-plus-g']} /></button>
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'facebook-f']} /></button>
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'github']} /></button>
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></button>
-                            </div>
                             <span>or use your email for registration</span>
-                            <input type="text" placeholder="Name" />
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <button className="sign-up-button">Sign Up</button>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                onChange={this.handleInputChange}
+                            />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                onChange={this.handleInputChange}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                onChange={this.handleInputChange}
+                            />
+                            <button
+                                className="sign-up-button"
+                                onClick={this.handleSignUp}
+                            >
+                                Sign Up
+                            </button>
                         </form>
                     </div>
                     <div className="form-container sign-in">
                         <form>
                             <h1>Sign In</h1>
-                            <div className="social-icons">
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'google-plus-g']} /></button>
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'facebook-f']} /></button>
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'github']} /></button>
-                                <button className="icon"><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></button>
-                            </div>
                             <span>or use your email password</span>
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <button className="forgot-password-button">Forget Your Password?</button>
-                            <button className="sign-in-button">Sign In</button>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                onChange={this.handleInputChange}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                onChange={this.handleInputChange}
+                            />
+                            <button
+                                className="forgot-password-button"
+                                onClick={this.handleForgotPassword}
+                            >
+                                Forget Your Password?
+                            </button>
+                            <button
+                                className="sign-in-button"
+                                onClick={this.handleSignIn}
+                            >
+                                Sign In
+                            </button>
                         </form>
                     </div>
                 </div>
