03b93e6e3157bba080a42982acd8d84b06d55301
Thanh Binh <binhnguyenn2410@gmail.com>
2023-12-07 20:28:27 +0700

    1
    1

--- src/Components/Home/Footer.js
+++ src/Components/Home/Footer.js
@@ -1,31 +1,31 @@
 import React from "react";
 import "./Footer.css";
 
-ClassName Footer extends React.Component {
+class Footer extends React.Component {
     render() {
         return (
             <div>
-                <div ClassName="container top-footer">
-                    <div ClassName="footer-item">
-                        <h2 ClassName="footer-title">ADDRESS</h2>
-                        <div ClassName="footer-items">
+                <div className="container top-footer">
+                    <div className="footer-item">
+                        <h2 className="footer-title">ADDRESS</h2>
+                        <div className="footer-items">
                             <h3>Ho Chi Minh</h3>
                             <h3>Ha Noi</h3>
                             <h3>Binh Duong</h3>
                         </div>
                     </div>
-                    <div ClassName="footer-item">
-                        <h2 ClassName="footer-title">SERVICES</h2>
-                        <div ClassName="footer-items">
+                    <div className="footer-item">
+                        <h2 className="footer-title">SERVICES</h2>
+                        <div className="footer-items">
                             <h3>All our services</h3>
                             <h3>FAQs</h3>
                             <h3>Contact us</h3>
                             <h3>Store Locator</h3>
                         </div>
                     </div>
-                    <div ClassName="footer-item">
-                        <h2 ClassName="footer-title">Legal & Corporate</h2>
-                        <div ClassName="footer-items">
+                    <div className="footer-item">
+                        <h2 className="footer-title">Legal & Corporate</h2>
+                        <div className="footer-items">
                             <h3>Terms of Use</h3>
                             <h3>Privacy Notice</h3>
                             <h3>Cookies Notice</h3>
@@ -33,18 +33,17 @@ ClassName Footer extends React.Component {
                             <h3>Jobs</h3>
                         </div>
                     </div>
-                    <div ClassName="footer-item">
-                        <h2 ClassName="footer-title">FOLOW US</h2>
-                        <div ClassName="footer-items">
+                    <div className="footer-item">
+                        <h2 className="footer-title">FOLLOW US</h2>
+                        <div className="footer-items">
                             <h3>Facebook</h3>
                             <h3>Tiktok</h3>
                             <h3>Youtube</h3>
                         </div>
                     </div>
                 </div>
-
             </div>
-        )
+        );
     }
 }
 
