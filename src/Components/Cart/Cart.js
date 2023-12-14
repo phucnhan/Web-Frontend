d7e41baa38d4cb3b014e8cb1a184f74c7b3f316c
Thanh Binh <binhnguyenn2410@gmail.com>
2023-12-15 00:45:20 +0700

    1
    1

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,122 +5,125 @@ import Navbar from "../Home/Navbar";
 import Footer from "../Home/Footer";
 
 class Cart extends React.Component {
-    state = {
-        cartItems: [
-            // Sample data, you may replace it with your actual cart items
-            { id: 1, name: "Anne Klein Watch", price: 150.0, quantity: 1 },
-            { id: 2, name: "Another Watch", price: 120.0, quantity: 2 },
-            { id: 3, name: "Third Watch", price: 180.0, quantity: 1 },
-        ],
-    };
+  state = {
+    cartItems: [
+      { id: 1, name: "Anne Klein Watch", price: 150.0, quantity: 1 },
+      { id: 2, name: "Another Watch", price: 120.0, quantity: 2 },
+      { id: 3, name: "Third Watch", price: 180.0, quantity: 1 },
+    ],
+  };
 
-    removeProduct = (productId) => {
-        this.setState((prevState) => ({
-            cartItems: prevState.cartItems.filter((item) => item.id !== productId),
-        }));
-    };
+  removeProduct = (productId) => {
+    this.setState((prevState) => ({
+      cartItems: prevState.cartItems.filter((item) => item.id !== productId),
+    }));
+  };
 
-    updateQuantity = (productId, newQuantity) => {
-        this.setState((prevState) => ({
-            cartItems: prevState.cartItems.map((item) =>
-                item.id === productId ? { ...item, quantity: newQuantity } : item
-            ),
-        }));
-    };
+  updateQuantity = (productId, newQuantity) => {
+    this.setState((prevState) => {
+      const updatedCartItems = prevState.cartItems.map((item) =>
+        item.id === productId ? { ...item, quantity: parseInt(newQuantity, 10) } : item
+      );
 
-    checkout = () => {
-        // Implement logic for the checkout process
-        // Redirect to a checkout page, show a confirmation message, or perform other necessary actions
-        alert("Checkout functionality will be implemented here.");
-    };
+      return {
+        cartItems: updatedCartItems,
+      };
+    });
+  };
 
-    render() {
-        const { cartItems } = this.state;
+  checkout = () => {
+    alert("Checkout functionality will be implemented here.");
+  };
 
-        return (
-            <div>
-                <Navbar />
-                <section className="cart">
-                    <h2>CART</h2>
-                    <div className="cart-table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Product</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                    <th>Total</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {cartItems.map((product) => (
-                                    <tr key={product.id}>
-                                        <td className="product">
-                                            <img
-                                                src={`stories/virtuemart/category/image_category_${product.id}.png`}
-                                                alt={product.name}
-                                                className="product-image"
-                                            />
-                                            <div className="product-details">
-                                                <h2 className="product-title">{product.name}</h2>
-                                                {/* Additional product details */}
-                                            </div>
-                                        </td>
-                                        <td>&euro; {product.price.toFixed(2)}</td>
-                                        <td>
-                                            <input
-                                                type="number"
-                                                value={product.quantity}
-                                                onChange={(e) =>
-                                                    this.updateQuantity(product.id, e.target.value)
-                                                }
-                                                min={1}
-                                            />
-                                        </td>
-                                        <td>&euro; {(product.price * product.quantity).toFixed(2)}</td>
-                                        <td>
-                                            <button
-                                                className="cart-btn remove-btn"
-                                                onClick={() => this.removeProduct(product.id)}
-                                            >
-                                                Remove
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="summary-table">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>Total Items:</td>
-                                    <td>{cartItems.length}</td>
-                                </tr>
-                                <tr>
-                                    <td>Total Price:</td>
-                                    <td>
-                                        &euro;{" "}
-                                        {cartItems.reduce(
-                                            (total, product) =>
-                                                total + product.price * product.quantity,
-                                            0
-                                        ).toFixed(2)}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button className="cart-btn checkout-btn" onClick={this.checkout}>
-                            CHECKOUT
-                        </button>
-                    </div>
-                </section>
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    const { cartItems } = this.state;
+
+    return (
+      <div>
+        <Navbar />
+        <section className="cart">
+          <h2>CART</h2>
+          <div className="cart-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((product) => (
+                  <tr key={product.id}>
+                    <td className="product">
+                      <img
+                        src={`stories/virtuemart/category/image_category_${product.id}.png`}
+                        alt={product.name}
+                        className="product-image"
+                      />
+                      <div className="product-details">
+                        <h2 className="product-title">{product.name}</h2>
+                      </div>
+                    </td>
+                    <td>&euro; {product.price.toFixed(2)}</td>
+                    <td>
+                      <input
+                        type="number"
+                        value={product.quantity}
+                        onChange={(e) => this.updateQuantity(product.id, e.target.value)}
+                        min={1}
+                      />
+                    </td>
+                    <td>&euro; {(product.price * product.quantity).toFixed(2)}</td>
+                    <td>
+                      <button
+                        className="cart-btn remove-btn"
+                        onClick={() => this.removeProduct(product.id)}
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="summary-table">
+            <table>
+              <tbody>
+                <tr>
+                  <td>Total Items:</td>
+                  <td>
+                    {cartItems.reduce(
+                      (totalItems, product) => totalItems + product.quantity,
+                      0
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Total Price:</td>
+                  <td>
+                    &euro;{" "}
+                    {cartItems.reduce(
+                      (total, product) => total + product.price * product.quantity,
+                      0
+                    ).toFixed(2)}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <button className="cart-btn checkout-btn" onClick={this.checkout}>
+              CHECKOUT
+            </button>
+          </div>
+        </section>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Cart;
+
