e30fa2f8ea5d9f3483e601d14c6026a829ed5907
Thanh Binh <binhnguyenn2410@gmail.com>
2023-12-15 08:58:40 +0700

    1
    1

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,129 +1,141 @@
 // Cart.js
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import "./Cart.css";
 import Navbar from "../Home/Navbar";
 import Footer from "../Home/Footer";
 
-class Cart extends React.Component {
-  state = {
-    cartItems: [
-      { id: 1, name: "Anne Klein Watch", price: 150.0, quantity: 1 },
-      { id: 2, name: "Another Watch", price: 120.0, quantity: 2 },
-      { id: 3, name: "Third Watch", price: 180.0, quantity: 1 },
-    ],
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    fetchCartData();
+  }, []);
+
+  const fetchCartData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/cart'); // Adjust the endpoint accordingly
+      setCartItems(response.data);
+    } catch (error) {
+      console.error('Error fetching cart data:', error);
+    }
   };
 
-  removeProduct = (productId) => {
-    this.setState((prevState) => ({
-      cartItems: prevState.cartItems.filter((item) => item.id !== productId),
-    }));
+  const removeProduct = async (productId) => {
+    try {
+      await axios.delete(`http://localhost:3001/api/remove-from-cart/${productId}`);
+      fetchCartData(); // Fetch updated cart data after removal
+    } catch (error) {
+      console.error('Error removing product from cart:', error);
+    }
   };
 
-  updateQuantity = (productId, newQuantity) => {
-    this.setState((prevState) => {
-      const updatedCartItems = prevState.cartItems.map((item) =>
-        item.id === productId ? { ...item, quantity: parseInt(newQuantity, 10) } : item
-      );
-
-      return {
-        cartItems: updatedCartItems,
-      };
-    });
+  const updateQuantity = async (productId, newQuantity) => {
+    try {
+      await axios.put('http://localhost:3001/api/update-cart-item', {
+        productId,
+        quantity: newQuantity,
+      });
+      fetchCartData(); // Fetch updated cart data after quantity update
+    } catch (error) {
+      console.error('Error updating quantity:', error);
+    }
   };
 
-  checkout = () => {
-    alert("Checkout functionality will be implemented here.");
+  const checkout = async () => {
+    try {
+      await axios.post('http://localhost:3001/api/checkout');
+      fetchCartData(); // Fetch updated cart data after checkout
+    } catch (error) {
+      console.error('Error during checkout:', error);
+    }
   };
 
-  render() {
-    const { cartItems } = this.state;
-
-    return (
-      <div>
-        <Navbar />
-        <section className="cart">
-          <h2>CART</h2>
-          <div className="cart-table">
-            <table>
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Total</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((product) => (
-                  <tr key={product.id}>
-                    <td className="product">
-                      <img
-                        src={`stories/virtuemart/category/image_category_${product.id}.png`}
-                        alt={product.name}
-                        className="product-image"
-                      />
-                      <div className="product-details">
-                        <h2 className="product-title">{product.name}</h2>
-                      </div>
-                    </td>
-                    <td>&euro; {product.price.toFixed(2)}</td>
-                    <td>
-                      <input
-                        type="number"
-                        value={product.quantity}
-                        onChange={(e) => this.updateQuantity(product.id, e.target.value)}
-                        min={1}
-                      />
-                    </td>
-                    <td>&euro; {(product.price * product.quantity).toFixed(2)}</td>
-                    <td>
-                      <button
-                        className="cart-btn remove-btn"
-                        onClick={() => this.removeProduct(product.id)}
-                      >
-                        Remove
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="summary-table">
-            <table>
-              <tbody>
-                <tr>
-                  <td>Total Items:</td>
+  return (
+    <div>
+      <Navbar />
+      <section className="cart">
+        <h2>CART</h2>
+        <div className="cart-table">
+          {/* Table for rendering cart items */}
+          <table>
+            {/* Table headers */}
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {/* Table body */}
+            <tbody>
+              {cartItems.map((product) => (
+                <tr key={product.id}>
+                  <td className="product">
+                    {/* Product details */}
+                    <div className="product-details">
+                      <h2 className="product-title">{product.name}</h2>
+                    </div>
+                  </td>
+                  <td>&euro; {product.price.toFixed(2)}</td>
                   <td>
-                    {cartItems.reduce(
-                      (totalItems, product) => totalItems + product.quantity,
-                      0
-                    )}
+                    <input
+                      type="number"
+                      value={product.quantity}
+                      onChange={(e) => updateQuantity(product.id, e.target.value)}
+                      min={1}
+                    />
+                  </td>
+                  <td>&euro; {(product.price * product.quantity).toFixed(2)}</td>
+                  <td>
+                    <button
+                      className="cart-btn remove-btn"
+                      onClick={() => removeProduct(product.id)}
+                    >
+                      Remove
+                    </button>
                   </td>
                 </tr>
-                <tr>
-                  <td>Total Price:</td>
-                  <td>
-                    &euro;{" "}
-                    {cartItems.reduce(
-                      (total, product) => total + product.price * product.quantity,
-                      0
-                    ).toFixed(2)}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <button className="cart-btn checkout-btn" onClick={this.checkout}>
-              CHECKOUT
-            </button>
-          </div>
-        </section>
-        <Footer />
-      </div>
-    );
-  }
-}
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/* Summary table */}
+        <div className="summary-table">
+          <table>
+            <tbody>
+              <tr>
+                <td>Total Items:</td>
+                <td>
+                  {cartItems.reduce(
+                    (totalItems, product) => totalItems + product.quantity,
+                    0
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Total Price:</td>
+                <td>
+                  &euro;{" "}
+                  {cartItems.reduce(
+                    (total, product) => total + product.price * product.quantity,
+                    0
+                  ).toFixed(2)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {/* Checkout button */}
+          <button className="cart-btn checkout-btn" onClick={checkout}>
+            CHECKOUT
+          </button>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
 export default Cart;
-
--- src/Components/Shop/ProductList.js
+++ src/Components/Shop/ProductList.js
@@ -1,10 +1,9 @@
 // ProductList.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import './Product.css';
 
-const ProductList = () => {
+const ProductList = ({ fetchCartData }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -23,6 +22,21 @@ const ProductList = () => {
     fetchProducts();
   }, []);
 
+  const addToCart = async (productId) => {
+    try {
+      const response = await axios.post('http://localhost:3001/api/add-to-cart', {
+        productId,
+        quantity: 1,
+      });
+
+      console.log(response.data);
+      // Fetch updated cart data after adding the product
+      fetchCartData();
+    } catch (error) {
+      console.error('Error adding to cart:', error);
+    }
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -36,22 +50,21 @@ const ProductList = () => {
       <ul>
         {products.map((product) => (
           <li key={product.id}>
-            <Link to={`/view-products/${product.id}`}>
-              <div className='product'>
-                <img
-                  src={`http://localhost:3001/images/${product.image_path}`}
-                  alt={product.name}
-                  style={{ width: '200px', height: '200px' }}
-                />
-                <span className='product_add_cart'>ADD TO CART</span>
-              </div>
-              <div>
-                <h3>{product.name}</h3>
-                <p>{product.description}</p>
-                <p>${product.price}</p>
-
-              </div>
-            </Link>
+            <div className='product'>
+              <img
+                src={`http://localhost:3001/images/${product.image_path}`}
+                alt={product.name}
+                style={{ width: '200px', height: '200px' }}
+              />
+              <button onClick={() => addToCart(product.id)} className='product_add_cart'>
+                ADD TO CART
+              </button>
+            </div>
+            <div>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p>${product.price}</p>
+            </div>
           </li>
         ))}
       </ul>
